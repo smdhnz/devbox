abe48ac86aa788b5295296b590de4182c8eb0005
smdhnz <strikerfmy1208@gmail.com>
2026-02-25 21:50:15 +0900

    style: change hostname color to cyan in PS1

--- bash_exports.sh
+++ bash_exports.sh
@@ -1,2 +1,2 @@
 export PATH="$BUN_INSTALL/bin:$VOLTA_HOME/bin:$PATH"
-export PS1='\[\033[01;32m\]\u@devbox\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+export PS1='\[\033[01;32m\]\u@\[\033[01;36m\]devbox\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
